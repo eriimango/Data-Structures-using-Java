0b9c0091a487a1033cbf66e37503535731d2e338
Ericka <64378078+eriimango@users.noreply.github.com>
2021-09-12 16:26:22 +0000

    Initial commit

---
+++ InsertionSort.java
@@ -0,0 +1,30 @@
+public class InsertionSort {
+  public static void insertionSort(int array[]) {
+    int n = array.length;
+    for (int j = 1; j < n; j++) {
+      int key = array[j];
+      int i = j - 1;
+      while ((i > -1) && (array[i] > key)) {
+        array[i + 1] = array[i];
+        i--;
+      }
+      array[i + 1] = key;
+    }
+  }
+
+  public static void main(String a[]) {
+    int[] arr = { 8, 22, 5, 15, 18, 32, 3, 72 };
+    System.out.println("Unsorted Array: ");
+    for (int i : arr) {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+
+    insertionSort(arr);
+
+    System.out.println("Sorted Array: ");
+    for (int i : arr) {
+      System.out.print(i + " ");
+    }
+  }
+}
