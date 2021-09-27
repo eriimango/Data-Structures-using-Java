f3f3f108e6a0198b60675a84db7d5eabf528f399
Ericka <64378078+eriimango@users.noreply.github.com>
2021-09-26 23:44:19 -0400

    Add files via upload

---
+++ MagicSquare.java
@@ -0,0 +1,46 @@
+// This program will implement a magic square odd integer intake. The matrix structure will move index
+// upperbounds, and if the space is occupied the index will move down.
+// author: Ericka Howard
+
+class MagicSquare {
+    static void magic(int n) {
+        int [][] magicSquare = new int[n][n];
+
+        // position 1
+        int j = 0;
+        int i = n / 2;
+
+        // next index(s) loop condition
+        for (int x = 1; x <= n * n; x++) {
+            magicSquare[j][i] = x;
+
+
+            // if space is occupied move down
+            if (x % n == 0)
+                j++;
+            else
+                // subtracting 1 to move upperbounds
+                j--;
+            i--;
+            // continue generation during upperbounds x
+            if (j < 0)
+                j ++;
+            if (i < 0)
+                i ++;
+        }
+
+
+        for (j = 0; j < n; j++) {
+            for (i = 0; i < n; i++) {
+                System.out.print(magicSquare[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        magic(n);
+    }
+}
+
