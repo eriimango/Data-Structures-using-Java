01481585b2c3794697d588607d5958b36c475157
Ericka <64378078+eriimango@users.noreply.github.com>
2021-09-27 00:14:50 -0400

    Update MagicSquare.java

--- MagicSquare.java
+++ MagicSquare.java
@@ -19,17 +19,17 @@ class MagicSquare {
             if (x % n == 0)
                 j++;
             else
-                // subtracting 1 to move upperbounds
+                // og increment position when empty
                 j--;
-            i--;
-            // continue generation during upperbounds x
+                i--;
+            // conditions to place outbounds value
             if (j < 0)
-                j ++;
+                j +=n;
             if (i < 0)
-                i ++;
+                i +=n;
         }
 
-        // print magic square including upperbound results of pushing empty squares
+        // print magic square
         for (j = 0; j < n; j++) {
             for (i = 0; i < n; i++) {
                 System.out.print(magicSquare[j][i] + " ");
@@ -37,17 +37,20 @@ class MagicSquare {
             System.out.println();
         }
     }
-    
-    // test driver to test an odd number
+
+    // user integer prompt and program driver
 
     public static void main(String[] args) {
-      
-     Scanner myNum = new Scanner(System.in);  // Create a Scanner object(num)
-    System.out.println("Enter a number: ");
 
-    int magicNum = myNum.nextLine();  // Read user input
-    System.out.println("Magic Square for : " + magicNum);  // Output square number
-    System.out.println("Sum of each row or column " + n * (n * n + 1) / 2 + ":"); // displays the constant sum
-    magic(n);
+        int n; // initialize n variable
+
+        System.out.println("Enter a number: "); // user prompt
+        Scanner myNum = new Scanner(System.in);  // Create a Scanner object(num)
+
+        n = myNum.nextInt(); // reads the next integer from the screen
+
+        System.out.println("Magic Square for : " + n);  // Output square number
+        System.out.println("Sum of each row or column " + n * (n * n + 1) / 2 + ":"); // displays the constant sum
+        magic(n);
     }
 }
